71e89fdc9d27846eceb437f6d8260cfaa8e4fcd4
Kaoru Nishimura <kaorunishimura@kaorunoMac-mini.local>
2026-02-09 20:09:45 +0900

    Deploy: update web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1720030402"
+    serviceWorkerVersion: "1521761129"
   }
 });
